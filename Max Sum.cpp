2c97bc7bfc3c00a3c673768fe4a9251bdd4e0b53
NeonTEevee <78305852+NeonTEevee@users.noreply.github.com>
2021-03-11 15:59:47 +0700

    Being work on the 1st part of "Max Sum"

---
+++ Max Sum.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main ()
+{
+	int i, j, x, y, b, max = 0;
+	scanf("%d", &x);
+	scanf("%d", &y);
+	
+	int a[x][x];
+	for(i=0; i<x; i++)
+	{
+		for(j=0; j<x; j++)
+		{
+			scanf("%d", &a[i][j]);
+		}
+	}
+	
+}
